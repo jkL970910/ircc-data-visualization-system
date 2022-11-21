87e2fa45644e2f568d537d8a9da7743e1db4e1ae
jkL970910 <jkliu97@gmail.com>
2022-11-21 01:10:38 -0500

    specify user fields

--- login/serializer.py
+++ login/serializer.py
@@ -4,4 +4,4 @@ from .models import UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = "__all__"
+        fields = ["user_id", "username", "first_name", "last_name", "email", "is_staff", "full_name", "user_gender", "user_birthdate", "user_address", "user_phone", "user_icon", "user_postcode"]
