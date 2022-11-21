e18cd2aee68f1f9774823aa2117ba826342623dd
jkL970910 <jkliu97@gmail.com>
2022-11-21 17:11:23 -0500

    seperate modal

--- login/serializer.py
+++ login/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import UserProfile
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializerLogin(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = "__all__"
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ["user_id", "username", "first_name", "last_name", "email", "is_staff", "full_name", "user_gender", "user_birthdate", "user_address", "user_phone", "user_icon", "user_postcode"]
