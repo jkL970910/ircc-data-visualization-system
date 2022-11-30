3efbea389e9728d63eee152bbb6a049a7d0ef8c8
jkL970910 <jkliu97@gmail.com>
2022-11-30 00:15:50 -0500

    update plan

--- plan/models.py
+++ plan/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid;
 
 class Plan(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(max_length=250, primary_key=True, default=uuid.uuid4, editable=False)
     plan_name = models.CharField(max_length=250, blank=True, unique=True, default="free")
     plan_price = models.CharField(max_length=250, blank=True, default="0")
     plan_description = models.CharField(max_length=250, blank=True, default="")
